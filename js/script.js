81dc7652a20141cd331b3c16dbb735f5273792a6
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-06-22 20:21:17 +0300

    7 block

---
+++ js/script.js
@@ -0,0 +1,57 @@
+new Swiper('.swip', {
+    // стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    // пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        // буллеты
+        clickable: true,
+        dynamicBullets: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+    },
+
+    slidesPerView: 3,
+
+    watchOverflow: true,
+
+    slidesPerGroup: 1,
+
+    centeredSlides: true,
+
+    initialSlide: 1,
+
+    loop: true,
+
+    speed: 800,
+
+    spaceBetween: 30,
+
+    // effect: 'coverflow',
+
+    // coverflowEffect: {
+    //     rotate: 20,
+    //     stretch: 50,
+    //     slideShadow: false,
+    // },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        }
+    },
+
+});
+
+

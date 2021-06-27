7ac4147d729f529a2c1833be2c4f88524b5e8749
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-06-27 14:43:38 +0300

    new our projects

--- js/script.js
+++ js/script.js
@@ -110,6 +110,63 @@ new Swiper('.swip3', {
 
 });
 
+new Swiper('.swip4', {
+    // стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    // пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        // буллеты
+        clickable: true,
+        dynamicBullets: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+    },
+
+    slidesPerView: 3,
+
+    watchOverflow: true,
+
+    slidesPerGroup: 1,
+
+    centeredSlides: true,
+
+    initialSlide: 1,
+
+    // loop: true,
+
+    speed: 800,
+
+    spaceBetween: 30,
+
+    // effect: 'coverflow',
+
+    // coverflowEffect: {
+    //     rotate: 20,
+    //     stretch: 50,
+    //     slideShadow: false,
+    // },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        }
+    },
+
+});
+
+
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
@@ -283,3 +340,15 @@ function openservices(block) {
 //     }
 // }
 
+
+// ПОДРОБНЕЕ
+
+function opentext(more) {
+    display = document.getElementById('more').style.display;
+    if (display == "none") {
+        document.getElementById('more').style.display = "block";
+        document.getElementById('btn-more').style.display = "none";
+    } else {
+        document.getElementById('more').style.display = "none";
+    }
+}

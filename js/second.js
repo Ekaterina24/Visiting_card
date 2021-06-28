d7ef144a5805397e319d5ffa3cd79e3b39634a5f
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-06-29 00:31:11 +0300

    second.html adaptive

--- js/second.js
+++ js/second.js
@@ -41,17 +41,20 @@ new Swiper('.swip', {
     breakpoints: {
         320: {
             slidesPerView: 1,
+            slidesPerColumn: 1,
         },
         480: {
             slidesPerView: 2,
+            slidesPerColumn: 1,
         },
         992: {
             slidesPerView: 3,
+            slidesPerColumn: 2,
         }
     },
 
-    slidesPerColumn: 2,
-
+    
+    
 });
 
 new Swiper('.swip2', {
@@ -97,3 +100,42 @@ new Swiper('.swip2', {
 
 
 });
+
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+const menuLinks = document.querySelectorAll('.main__menu__item[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header2').offsetHeight;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
